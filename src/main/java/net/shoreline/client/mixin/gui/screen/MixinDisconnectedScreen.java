88fb31721706c65620a1d0e8635fa9b976556d18
Vexyneo <smyj292513@gmail.com>
2026-02-20 12:02:48 +0900

    Update MixinDisconnectedScreen.java

--- src/main/java/net/shoreline/client/mixin/gui/screen/MixinDisconnectedScreen.java
+++ src/main/java/net/shoreline/client/mixin/gui/screen/MixinDisconnectedScreen.java
@@ -1,99 +1,57 @@
 package net.shoreline.client.mixin.gui.screen;
 
 import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.text.Text;
+import net.shoreline.client.impl.module.misc.HeadlessMCModule;
+import net.shoreline.client.init.Managers;
 import net.shoreline.client.util.Globals;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * @author linus
- * @since 1.0
+ * MixinDisconnectedScreen - HeadlessMC AutoReconnect + 수동 Reconnect 버튼.
+ *
+ * DisconnectedScreen에 "Reconnect" 버튼을 추가한다.
+ * - HeadlessMC AutoReconnect 활성: 자동 재접속 (버튼 클릭으로도 즉시 재접속)
+ * - HeadlessMC 비활성: 버튼 클릭으로 수동 재접속
  */
 @Mixin(DisconnectedScreen.class)
 public abstract class MixinDisconnectedScreen extends MixinScreen implements Globals
 {
-//    @Shadow
-//    @Final
-//    private DirectionalLayoutWidget grid;
-//    @Shadow
-//    @Final
-//    private Text reason;
-//    @Shadow
-//    @Final
-//    private Text buttonLabel;
-//    @Shadow
-//    @Final
-//    private Screen parent;
-//    @Shadow
-//    @Final
-//    private static Text TO_TITLE_TEXT;
-//
-//    @Shadow
-//    protected abstract void initTabNavigation();
-//
-//    //
-//    @Unique
-//    private long reconnectSeconds;
-//
-//    /**
-//     * @param ci
-//     */
-//    @Inject(method = "init", at = @At(value = "HEAD"), cancellable = true)
-//    private void hookInit(CallbackInfo ci) {
-//        ci.cancel();
-//        grid.getMainPositioner().alignHorizontalCenter().margin(2);
-//        ButtonWidget.Builder reconnectButton = ButtonWidget.builder(Text.of("Reconnect"),
-//                (button) ->
-//                {
-//                    if (Managers.NETWORK.getAddress() != null && Managers.NETWORK.getInfo() != null) {
-//                        ConnectScreen.connect((DisconnectedScreen) (Object) this, mc,
-//                                Managers.NETWORK.getAddress(), Managers.NETWORK.getInfo(), false);
-//                    }
-//                });
-//        ButtonWidget.Builder autoReconnectButton = ButtonWidget.builder(
-//                Text.of("AutoReconnect"), (button) ->
-//                {
-//                    Modules.AUTO_RECONNECT.toggle();
-//                    if (Modules.AUTO_RECONNECT.isEnabled()) {
-//                        reconnectSeconds = Math.round(Modules.AUTO_RECONNECT.getDelay() * 20.0f);
-//                    }
-//                });
-//        grid.add(new TextWidget(title, mc.textRenderer));
-//        grid.add(new MultilineTextWidget(reason, mc.textRenderer).setMaxWidth(width - 50).setCentered(true));
-//        ButtonWidget.Builder buttonWidget = mc.isMultiplayerEnabled() ? ButtonWidget.builder(buttonLabel, button -> mc.setScreen(parent)) : ButtonWidget.builder(TO_TITLE_TEXT, button -> mc.setScreen(new TitleScreen()));
-//        grid.add(reconnectButton.build());
-//        grid.add(autoReconnectButton.build());
-//        grid.add(buttonWidget.build());
-//        grid.refreshPositions();
-//        grid.forEachChild(this::addDrawableChild);
-//        initTabNavigation();
-//        // addDrawableChild(reconnectButton.dimensions(width / 2 - 100,
-//        //        height / 2 + mc.textRenderer.fontHeight + 24, 200, 20).build());
-//        // addDrawableChild(autoReconnectButton.dimensions(width / 2 - 100,
-//        //        height / 2 + mc.textRenderer.fontHeight + 48, 200, 20).build());
-//        if (Modules.AUTO_RECONNECT.isEnabled()) {
-//            reconnectSeconds = Math.round(Modules.AUTO_RECONNECT.getDelay() * 20.0f);
-//        }
-//    }
-//
-//    /**
-//     *
-//     */
-//    @Override
-//    public void tick() {
-//        super.tick();
-//        if (getDrawables().size() > 1) {
-//            if (Modules.AUTO_RECONNECT.isEnabled()) {
-//                ((AccessorClickableWidget) getDrawables().get(3)).setMessage(
-//                        Text.of("AutoReconnect (" + (reconnectSeconds / 20 + 1) + ")"));
-//                if (reconnectSeconds > 0) {
-//                    --reconnectSeconds;
-//                } else if (Managers.NETWORK.getAddress() != null && Managers.NETWORK.getInfo() != null) {
-//                    ConnectScreen.connect((DisconnectedScreen) (Object) this, mc,
-//                            Managers.NETWORK.getAddress(), Managers.NETWORK.getInfo(), false);
-//                }
-//            } else {
-//                ((AccessorClickableWidget) getDrawables().get(3)).setMessage(Text.of("AutoReconnect"));
-//            }
-//        }
-//    }
+    @Shadow @Final private Screen parent;
+    @Shadow @Final private DirectionalLayoutWidget layout;
+
+    @Shadow protected abstract void initTabNavigation();
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void hookInit(CallbackInfo ci)
+    {
+        // "Reconnect" 버튼 - DisconnectedScreen 하단에 추가
+        addDrawableChild(ButtonWidget.builder(
+                Text.literal("Reconnect"),
+                button -> manualReconnect())
+                .dimensions(width / 2 - 100, height - 52, 200, 20)
+                .build());
+    }
+
+    @Unique
+    private void manualReconnect()
+    {
+        var addr = Managers.NETWORK.getAddress();
+        var info = Managers.NETWORK.getInfo();
+        if (addr != null && info != null)
+        {
+            ConnectScreen.connect(new TitleScreen(), mc, addr, info, false, null);
+        }
+    }
 }
